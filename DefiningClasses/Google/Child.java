8890cf1c7194e823e86a394374943a31e7f644c0
vladimiruzunov <uzunovladimir@gmail.com>
2021-03-18 19:41:22 +0200

    Adding new exercises

---
+++ DefiningClasses/Google/Child.java
@@ -0,0 +1,16 @@
+package Google;
+
+public class Child {
+    private String name;
+    private String birthDate;
+
+    public Child(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.birthDate + System.lineSeparator();
+    }
+}
---
+++ DefiningClasses/Google/Parent.java
@@ -0,0 +1,16 @@
+package Google;
+
+public class Parent {
+    private String name;
+    private String birthDate;
+
+    public Parent(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " " + this.birthDate + System.lineSeparator();
+    }
+}

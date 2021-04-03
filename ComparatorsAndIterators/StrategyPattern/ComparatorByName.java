a529e06b46099133dc226a4b876a5ae7f57b6bc6
vladimiruzunov <uzunovladimir@gmail.com>
2021-04-03 11:58:18 +0300

    Adding StrategyPattern

---
+++ ComparatorsAndIterators/StrategyPattern/ComparatorByName.java
@@ -0,0 +1,16 @@
+package StrategyPattern;
+
+import java.util.Comparator;
+
+public class ComparatorByName implements Comparator<Person> {
+    @Override
+    public int compare(Person p1, Person p2) {
+        int res = p1.getName().length() - p2.getName().length();
+        if (res == 0){
+            char p1FirstLetter = p1.getName().toLowerCase().charAt(0);
+            char p2FirstLetter = p2.getName().toLowerCase().charAt(0);
+            res = p1FirstLetter - p2FirstLetter;
+        }
+        return res;
+    }
+}
